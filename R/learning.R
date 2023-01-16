f1bbf0f4eef9b7071b5d34c1b7af6c577183baa0
Nathália Vilela <nathaliavilela@gmail.com>
2023-01-16 16:07:23 +0100

    Add initial TODO.md file.

---
+++ R/learning.R
@@ -0,0 +1,20 @@
+10
+
+# R basics ----------------------------------------------------------------
+
+weigth_kilos <- 100
+weigth_kilos <- 10
+
+colnames(airquality)
+
+str(airquality)
+
+summary(airquality)
+
+
+
+# Packages ----------------------------------------------------------------
+
+library(tidyverse)
+r3::check_git_config()
+
