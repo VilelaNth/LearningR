417d938c5d5935c9f6cb44ae46484076ad364e78
Nathália Vilela <nathaliavilela@gmail.com>
2023-01-17 11:15:58 +0100

    Made smaller nhanes dataset

--- R/learning.R
+++ R/learning.R
@@ -17,6 +17,33 @@ summary(airquality)
 
 library(tidyverse)
 r3::check_git_config()
-#This will be used to testing out Git
+# This will be used to testing out Git
+
+library(tidyverse)
+library(NHANES)
 
 
+# Looking at data ---------------------------------------------------------
+
+glimpse(NHANES)
+
+select(NHANES, Age, Weight, BMI)
+
+select(NHANES, -HeadCirc)
+select(NHANES, starts_with("BP"))
+
+select(NHANES, ends_with("Day"))
+select(NHANES, contains("Age"))
+
+nhanes_small <-
+  select(
+    NHANES,
+    Age,
+    Gender,
+    BMI,
+    Diabetes,
+    PhysActive,
+    BPSysAve,
+    BPDiaAve,
+    Education
+  )
