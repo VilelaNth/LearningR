0b7a71484ff96fa0f02404446348f895d254928c
Nathália Vilela <nathaliavilela@gmail.com>
2023-01-17 11:49:22 +0100

    Exercise 7.8

--- R/learning.R
+++ R/learning.R
@@ -72,3 +72,35 @@ nhanes_small %>%
 nhanes_small %>%
   select(phys_active) %>%
   rename(physically_active = phys_active)
+nhanes_small %>%
+  select(bp_sys_ave, education)
+nhanes_small %>%
+  rename(
+    bp_sys = bp_sys_ave,
+    bp_dia = bp_sys_ave
+  )
+nhanes_small %>%
+  select(bmi, contains("age"))
+nhanes_small %>%
+  select(starts_with("bp_")) %>%
+  rename(bp_systolic = bp_sys_ave)
+
+# 1. Select specific columns
+nhanes_small %>%
+  select(bp_sys_ave, education)
+
+# 2. Rename columns
+nhanes_small %>%
+  rename(
+    bp_sys = bp_sys_ave,
+    bp_dia = bp_dia_ave
+  )
+
+# 3. Re-write with pipe
+nhanes_small %>%
+  select(bmi, contains("age"))
+
+# 4. Re-write with pipe
+nhanes_small %>%
+  select(starts_with("bp_")) %>%
+  rename(bp_systolic = bp_sys_ave)
